acf888e6a0889634dab11356068582d3d8650220
Mark Evertz <mark.evertz@gmx.de>
2019-04-05 18:18:13 +0200

    added index.html

--- src/main/java/com/example/ameliane/AmelianeApplication.java
+++ src/main/java/com/example/ameliane/AmelianeApplication.java
@@ -9,13 +9,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+
 
 @SpringBootApplication
 @RestController
 public class AmelianeApplication {
 
+	
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(AmelianeApplication.class);
@@ -29,7 +34,14 @@ public class AmelianeApplication {
 		SpringApplication.run(AmelianeApplication.class, args);
 	}
 	
-	@RequestMapping("/")
+	
+	   @GetMapping("/greeting")
+	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	        model.addAttribute("name", name);
+	        return "greeting";
+	    }
+	
+	@RequestMapping("/echo")
 	public String echo(@RequestParam Map<String,String> allParams) {
 		return "I'm Ameliane. How can I help you: " + allParams.entrySet();
 	}
